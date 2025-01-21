f0daae6eb3cea291870deb6ab1771b2aaaae4774
Uparsin <etikii87@gmail.com>
2025-01-21 20:29:39 +0300

    Added some simple example validation
    Example of validation function to define if that's a credit card supported by the service and if it is that function specifies the credit card company

--- credit card.py
+++ credit card.py
@@ -1,3 +1,16 @@
+def card_validate(card):
+    # defines if that's American Express, MasterCard or Visa card
+    mastercardlist = ["51", "52", "53", "54", "55"]
+    if len(card) == 15:
+        return "American Express"
+    elif len(card) == 16 and card[0] == "4":
+        return "Visa"
+    elif len(card) == 13 or (len(card) == 16 and card[0:2] not in mastercardlist):
+        return  "MasterCard"
+    else:
+        return False
+
+
 def main():
     card = "".join(str(input("Please, enter your credit card number: ")).split())
     # reads input and removes every whitespace in it
@@ -9,10 +22,14 @@ def main():
         else:
             to_add = (int(v) * 2 // 10) + (int(v) * 2 % 10)
             luth_check += to_add
-    if luth_check % 10 != 0:
-        print("Your credit card number is invalid")
+
+    if card_validate(card):
+        print("Sorry, we don't support cards of your credit card company yet!")
     else:
-        print("Your credit card number is valid, proceed to payment")
+        if luth_check % 10 != 0:
+            print(f"Your {card_validate(card)} credit card number is invalid.")
+        else:
+            print(f"Your {card_validate(card)} credit card number is valid, proceed to payment.")
 
 if __name__ == "__main__":
     main()
